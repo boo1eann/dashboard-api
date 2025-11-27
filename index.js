e33cf833c795d0efa6e4294706068bb2878abdf0
Damir <ddevq31@gmail.com>
2025-11-27 21:11:03 +0500

    Промежуточные обработчики

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import { userRouter } from './users/users.js'
 
 const port = 8000
 const app = express()
 
+app.use((req, res, next) => {
+	console.log('Время ', Date.now())
+	next()
+})
+
 app.get('/hello', (req, res) => {
-	res.send('Привет!')
+	throw new Error('Error!!!')
+})
+
+app.use('/users', userRouter)
+
+app.use((err, req, res, next) => {
+	console.log(err.message)
+	res.status(401).send(err.message)
 })
 
 app.listen(port, () => {
